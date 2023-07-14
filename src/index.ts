4e86eaf91ca1e1cc1173aba444b1321e36d80417
Yaslin Carmona <yaslincarmona@Yaslins-MacBook-Air.local>
2023-07-13 21:06:21 -0400

    CREATED api

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+//@ts-ignore
+import {MongoClient} from "mongodb";
+const app = express();
+app.use(cors());
+app.use(express.json());
+const client = new MongoClient(process.env.MONGO_URI);
+const db = client.db("c11-practice");
+const collection = db.collection("ts-practice");
+app.post("/", async (req,res) => {
+  console.log(req.body);
+  const newDoc = await collection.insertOne(req.body);
+  res.status(200).send("Hello Uniervse");
+})
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
